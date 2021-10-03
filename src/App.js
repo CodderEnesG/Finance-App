22a9d0f3c4d604bf447ff4518f19f9bb0a35194e
Enes Güngör <70583416+CodderEnesG@users.noreply.github.com>
2021-10-03 16:19:51 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+  
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+import Coin from './Coin';
+
+function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+  const [formKey, setFormKey] = useState(10); //for reseting states
+
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+      )
+      .then(res => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    setSearch(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault()
+
+    setSearch("")  };
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input
+            className='coin-input'
+            type='text'
+            onChange={handleChange}
+            placeholder='Search'
+            value={search}
+          >        
+          </input>
+          <button className="clear" type="submit" onClick={handleClick}>C</button>
+          
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
